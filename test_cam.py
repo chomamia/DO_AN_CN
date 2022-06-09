1e3cae8af94b663d6dad8ee9d59573af052ad56e
chomamia <huuphuuongtp@gmail.com>
2022-06-09 12:42:51 +0700

    update 9-6-2022

--- test_cam.py
+++ test_cam.py
@@ -1,15 +1,16 @@
 import cv2
 import numpy as np
 from PIL import Image
-
+from tkinter.filedialog import askopenfilename
 import torch
 import torch.nn as nn
 import torchvision.transforms as transforms
 
 from src.models.modnet import MODNet
 
-
-
+background_path = askopenfilename()
+background = cv2.imread(background_path)
+background = cv2.resize(background, (672,512))
 torch_transforms = transforms.Compose(
     [
         transforms.ToTensor(),
@@ -53,7 +54,7 @@ while (True):
         _, _, matte_tensor = modnet(frame_tensor, True)
     matte_tensor = matte_tensor.repeat(1, 3, 1, 1)
     matte_np = matte_tensor[0].data.cpu().numpy().transpose(1, 2, 0)
-    fg_np = matte_np * frame_np + (1 - matte_np) * np.full(frame_np.shape, 255.0)
+    fg_np = matte_np * frame_np + (1 - matte_np) * background
     view_np = np.uint8(np.concatenate((frame_np, fg_np), axis=1))
     view_np = cv2.cvtColor(view_np, cv2.COLOR_RGB2BGR)
 
